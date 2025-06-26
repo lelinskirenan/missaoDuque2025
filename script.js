c73e622deb72207c312b0063c13f37ecea87a9ff
lelinskirenan <159435046+lelinskirenan@users.noreply.github.com>
2025-06-26 11:16:48 -0300

    Missão Perguntas

---
+++ script.js
@@ -0,0 +1,10 @@
+const caixaPricipal = document.querySelector(".caixa-Principal");
+const caixaPerguntas = document.querySelector(".caixa-Perguntas");
+const caixaAlternativas = document.querySelector(".caixa-Alternativas");
+const caixaResultado = document.querySelector(".caixa-Resultado");
+const textoResultado = document.querySelector(".texto-Resultado");
+
+const Perguntas = [ // array com todas as perguntas do quiz]
+    {
+        enunciado: "você está se preparando para a primeiro saque. qual a sua estrategia?"
+    }
