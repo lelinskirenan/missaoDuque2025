08812ac5e03930a35ca47368cb858a7f74f9a793
lelinskirenan <159435046+lelinskirenan@users.noreply.github.com>
2025-07-03 11:18:04 -0300

    missão

--- script.js
+++ script.js
@@ -4,7 +4,79 @@ const caixaAlternativas = document.querySelector(".caixa-Alternativas");
 const caixaResultado = document.querySelector(".caixa-Resultado");
 const textoResultado = document.querySelector(".texto-Resultado");
 
-const Perguntas = [ // array com todas as perguntas do quiz]
+const Perguntas = [
     {
-        enunciado: "você está se preparando para a primeiro saque. qual a sua estrategia?"
-    }
+        enunciado: "qual foi a maior pontuação e uma partida de ping pong?",
+        alternativas: [
+{
+ texto:"11-0",
+ afirmação:"resposta errada",
+ pontos:0
+},
+{
+    texto:"21-0",
+    afirmação:"resposta correta",
+    pontos:1
+}
+        ]
+    },
+       {
+        enunciado: "ganhador de 1994 de ping pong?",
+        alternativas: [
+{
+ texto:"Hugo Hoyama",
+ afirmação:"resposta correta",
+ pontos:1
+{
+    texto:"Hugo",
+    afirmação:"resposta errada",
+    pontos:0
+}
+        ]
+    },
+       {
+        enunciado: "maior jogador de ping pong da historia brasileira?",
+        alternativas: [
+{
+ texto:"Hugo Calderano",
+ afirmação:"resposta corrata",
+ pontos:1
+},
+{
+    texto:"Luca Kumahara",
+    afirmação:"resposta erreda",
+    pontos:0
+}
+        ]
+    },
+       {
+        enunciado: "qual foi a maior pontuação e uma partida de ping pong?",
+        alternativas: [
+{
+ texto:"11-0",
+ afirmação:"resposta errada",
+ pontos:0
+},
+{
+    texto:"21-0",
+    afirmação:"resposta correta",
+    pontos:1
+}
+        ]
+    },
+       {
+        enunciado: "qual foi a maior pontuação e uma partida de ping pong?",
+        alternativas: [
+{
+ texto:"11-0",
+ afirmação:"resposta errada",
+ pontos:0
+},
+{
+    texto:"21-0",
+    afirmação:"resposta correta",
+    pontos:1
+}
+        ]
+    },
+]
